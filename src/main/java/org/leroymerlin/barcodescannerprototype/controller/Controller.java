e5a0fbed0393bfcec1a4c7dd2f0acece0fd11f2f
gfiao <guilherme.fiao@gmail.com>
2018-07-30 17:43:45 +0100

    Decode base64 string to BufferedImage.
    return image resolution

--- src/main/java/org/leroymerlin/barcodescannerprototype/controller/Controller.java
+++ src/main/java/org/leroymerlin/barcodescannerprototype/controller/Controller.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sun.misc.BASE64Decoder;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 @RestController
 @AllArgsConstructor
@@ -15,9 +21,16 @@ public class Controller {
         return "Hello World!";
     }
 
-    @PostMapping(value = "/post")
-    public String helloPost(@RequestBody String name) {
-        return "Hello " + name + "!";
+    @PostMapping(value = "/process")
+    public String helloPost(@RequestBody String base64) throws IOException {
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] imageBytes = decoder.decodeBuffer(base64);
+
+        ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
+        BufferedImage image = ImageIO.read(bis);
+        bis.close();
+
+        return "Resolution: " + image.getWidth() + "x" + image.getHeight();
     }
 
 }
